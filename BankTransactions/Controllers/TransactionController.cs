0ac08b96a7786528cbbb34edd5eaaf4989869285
Hasibul Hasan <hhasan.cse@gmail.com>
2023-01-23 13:17:07 +0600

    BankTransactionSystem_v1.1

---
+++ BankTransactions/Controllers/TransactionController.cs
@@ -0,0 +1,68 @@
+﻿#nullable disable
+using BankTransactions.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+
+        // GET: Transaction/AddOrEdit
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Transaction());
+            else
+                return View(_context.Transactions.Find(id));
+        }
+
+        // POST: Transaction/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transaction transaction)
+        {
+            // Server Side Validation
+            if (ModelState.IsValid)
+            {
+                if (transaction.TransactionId == 0)
+                {
+                    transaction.Date = DateTime.Now;
+                    _context.Add(transaction);
+                }
+                else
+                    _context.Update(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
+
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ BankTransactions/Migrations/20230122080906_Second Version Migration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankTransactions.Migrations
+{
+    public partial class SecondVersionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Transactions",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Amount",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
